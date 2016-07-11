b98bec86cf51b40fb0886c489f5837cb22c1c157
flaredshadow <flaredlight@aim.com>
2016-07-10 23:30:46 -0700

    First Commit

---
+++ Grunt 3.0/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour {
+
+	Button buttonComponent;
+
+	// Use this for initialization
+	void Start () {
+		buttonComponent = GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Engine.self._getCurrentGameState() == GameState.Dialogue)
+		{
+			
+		}
+		
+	
+	}
+
+	public void _loadSaveFile(string nextSceneName)//should change or remove paramater to use deSerialization
+	{
+		Engine.self.worldPlayer.SetActive(true);
+		Engine.self._goToScene(nextSceneName);
+		Engine.self._setCurrentGameState(GameState.OverWorldPlay);
+	}
+
+
+}
---
+++ Grunt 3.0/Assets/Scripts/Engine.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public enum GameState {PickFile, Dialogue, CutScene, OverWorldPlay, BattlePlay, Ending, EnterScene, ExitScene}
+public class Engine : MonoBehaviour {
+
+	public static Engine self;
+
+	public GameObject worldPlayer;
+
+	GameState currentGameState = GameState.PickFile;
+	string currentScene;
+
+	// Use this for initialization
+	void Start () {
+		self = this;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string pickFileScene = "IntroScene";
+		switch(currentGameState)
+		{
+			case GameState.PickFile:
+				_goToScene(pickFileScene);
+				currentGameState = GameState.Dialogue;
+				break;
+			case GameState.Dialogue:
+				break;
+			case GameState.CutScene:
+				break;
+			case GameState.OverWorldPlay:
+				break;
+			case GameState.BattlePlay:
+				break;
+			case GameState.Ending:
+				break;
+		}
+	
+	}
+
+	public GameState _getCurrentGameState()
+	{
+		return currentGameState;
+	}
+
+	public void _setCurrentGameState(GameState givenState)
+	{
+		currentGameState = givenState;
+	}
+
+	public void _goToScene(string nextSceneName)
+	{
+		SceneManager.UnloadScene(currentScene);
+		SceneManager.LoadScene(nextSceneName, LoadSceneMode.Additive);
+		currentScene = nextSceneName;
+	}
+}
